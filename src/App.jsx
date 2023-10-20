70fe6b6f653e9722ceb4876faa1f5b7dfe6cb2c9
AbuDzarAlGhifari <rasduba@gmail.com>
2023-10-21 06:35:50 +0700

    Perbaiki UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './index.css';
 import {getMovieList, searchMovie} from "./components/api"
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
 const [popularMovies, setPopularMovies] = useState([])
@@ -14,11 +15,11 @@ const [popularMovies, setPopularMovies] = useState([])
   const PopularMovieList = () => {
     return popularMovies.map((movie, i) => {
       return (
-          <div className='Movie-wrapper' key={i}>
-            <div className='Movie-title'>{movie.title}</div>
-            <img className='Movie-image' src={`${import.meta.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}/>
-            <div className='Movie-date'>{movie.release_date}</div>
-            <div className='Movie-rate'>{movie.vote_average}</div>
+          <div className='bg-gray-900 p-3 rounded-2xl shadow-lg hover:-translate-y-1 border-2 border-gray-200 ' key={i}>
+            <img className='rounded-lg w-full h-full mb-2 ' src={`${import.meta.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}/>
+            <div className='text-xl text-white font-bold text-center '>{movie.title}</div>
+            <div className='text-sm text-gray-300 font-medium text-center'>Release : {movie.release_date}</div>
+            <div className='text-sm text-red-700 font-bold text-center'>{movie.vote_average}</div>
           </div>
       )
     })
@@ -32,21 +33,24 @@ const [popularMovies, setPopularMovies] = useState([])
   }
 
   return (
-    <div className="App"> 
-      <header className="App-header">
+    <div className='bg-gray-600'>
+      <div >
+        < Navbar />
+      </div>
 
-      <nav className="navbar">
+      <div className="input flex justify-center  px-5 lg:px-0 py-5">
           <input 
           placeholder='Cari film....' 
-          className='Movie-search'
+          className='shadow-md bg-gray-200 placeholder-gray-400 rounded-lg 
+          px-2 py-2 outline-none border-2 border-gray-500 text-black text-center'
           onChange={({target}) => search(target.value)}
           />
-      </nav>
+      </div>
 
-        <div className='Movie-container'>
-          <PopularMovieList />
-        </div>
-      </header>
+      <div className='Movie-container'>
+        <PopularMovieList />
+      </div>
+
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,9 @@
 function Navbar() {
     return (
         <div className='main z-20 lg:flex md:flex flex-wrap justify-between items-center 
-     px-4 bg-[#40407a] py-4 shadow-md sticky top-0'>
+     px-4 bg-gray-900 py-4 shadow-md sticky top-0'>
             <div className="left flex items-center space-x-3">
-                <img className='w-10' src="https://movie-app-88kamal.vercel.app/clipart3105859.png" alt="" />
-                <h2 className="logo font-bold text-2xl text-white text-center">MovieMaSala</h2>
+                <h2 className="logo font-bold text-2xl text-white text-center">MovieKu</h2>
             </div>
             <div className="right">
                 <ul className='flex space-x-4 text-white justify-center'>
