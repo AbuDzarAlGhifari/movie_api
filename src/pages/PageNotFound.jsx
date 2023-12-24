308890cbe12992b166e424d5fe305c9b3c2cca3b
AbuDzarAlGhifari <rasduba@gmail.com>
2023-12-24 08:38:34 +0700

    Perbaiki Page Not Found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -4,20 +4,27 @@ import { useNavigate } from "react-router";
 function PageNotFound() {
   const [time, setTime] = useState(5);
   const navigate = useNavigate();
+
   useEffect(() => {
-    setTimeout(() => {
-      setTime(time - 1);
-      if (time === 0) {
-        navigate("/", { replace: true });
-      }
+    const timerId = setInterval(() => {
+      setTime((prevTime) => prevTime - 1);
     }, 1000);
+
+    return () => clearInterval(timerId);
+  }, []);
+
+  useEffect(() => {
+    if (time === 0) {
+      navigate(-1);
+    }
   }, [time, navigate]);
+
   return (
-    <div className='bg-gray-700 h-screen flex flex-col items-center justify-center text-8xl font-bold'>
-      <div className='text-red-600'>404</div>
-      <div className='text-4xl text-white'>Page Not Found</div>
-      <div className='text-4xl text-white'>
-        Redirecting to home page in {time} sec
+    <div className="bg-gray-900 h-screen flex flex-col items-center justify-center text-lg sm:text-xl lg:text-4xl font-bold">
+      <div className="text-xl sm:text-4xl lg:text-6xl text-red-700">404</div>
+      <div className="text-white font-poppins">Page Not Found</div>
+      <div className="text-white font-poppins">
+        Redirecting to the previous page in {time} sec
       </div>
     </div>
   );
